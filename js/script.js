2f777ecb1ff97067f7fbc755a1db9d38cb512436
Paloma Barroso <palomapbarroso@gmail.com>
2019-07-25 15:38:35 -0300

    Ajustes na página de interesses.
    Adição de imagens e ajustes na página de interesses.

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ function buscaPagina(p){
     let pagina = p;
     window.open(`${pagina}.html`, '_self') ;
 }
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
