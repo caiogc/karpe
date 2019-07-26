1faa73c1c82500a7bfb6e1fb0cc4dec9b97421b4
Paloma Barroso <palomapbarroso@gmail.com>
2019-07-26 08:06:09 -0300

    Adição de modal bootstrap no fim da compra.
    Adição de modal bootstrap no fim da compra.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,6 @@
 let pagina = ''
 
-function buscaPagina(p){
-    let pagina = p;
-    window.open(`${pagina}.html`, '_self') ;
+function buscaPagina(p) {
+  let pagina = p;
+  window.open(`${pagina}.html`, '_self');
 }
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
